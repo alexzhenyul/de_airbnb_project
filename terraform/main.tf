d8fe38ce64b968b2d1d283e6a814111b0baf6f78
alexzhenyul <alex.zhenyul@gmail.com>
2023-07-12 00:46:39 +1000

    updated terrafrom files

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,7 +77,48 @@ resource "aws_instance" "airbnb_de_ec2" {
     instance_type   = var.instance_type
     key_name        = aws_key_pair.generated_key.key_name
     security_groups = [aws_security_group.sde_security_group.name]
+
+    user_data = <<EOF
+#!/bin/bash
+
+echo "-------------------------START SETUP---------------------------"
+sudo apt-get -y update
+
+sudo apt-get -y install \
+ca-certificates \
+curl \
+gnupg \
+lsb-release
+
+sudo apt -y install unzip
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo chmod 666 /var/run/docker.sock
+
+sudo apt install make
+
+echo 'Clone git repo to EC2'
+cd /home/ubuntu && git clone ${var.repo_url}
+
+echo 'CD to data_engineering_project_template directory'
+cd data_engineering_project_template
+
+echo 'Start containers & Run db migrations'
+make up
+
+echo "-------------------------END SETUP---------------------------"
+
+EOF
+
 }
+
 
 
 
